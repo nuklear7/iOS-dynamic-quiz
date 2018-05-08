4ba018899120b578c2e3a6dab928a44e38110e41
Mohácsi Dávid <david.mohacsi@suitsolutions.eu>
2018-05-08 22:09:37 +0200

    Scoring development.

---
+++ EzKuiz/Supporting Files/QuizTypes.swift
@@ -0,0 +1,13 @@
+//
+//  QuizTypes.swift
+//  EzKuiz
+//
+//  Created by SuIT Solutions on 2018. 05. 07..
+//  Copyright © 2018. Suit Solutions. All rights reserved.
+//
+
+import UIKit
+
+class QuizTypes: NSObject {
+
+}
